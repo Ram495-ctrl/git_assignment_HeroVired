bdc951e01b5002b06619a95e5002d27d2bbfdf92
Ram495-ctrl <ramasrinivas495@gmail.com>
2024-07-16 00:25:42 +0530

    testing completed in dev branch

--- CalculatorPlus.py
+++ CalculatorPlus.py
@@ -22,7 +22,7 @@ raise ValueError("Cannot divide by zero.")
 
 return a / b
 
-# TODO: Implement the following function to calculate the square root of a number.
+# TODO:    Implement the following function to calculate the square root of a number.
 
 # def square_root(self, x):
 
